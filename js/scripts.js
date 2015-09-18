2b06a301d414fe6af9f51b08944c58f0ddd765da
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-09-18 11:05:30 -0700

    Adds spec and passing code to create a pizza with quantity, size, name, and toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-
+function Pizza(pizzaName, pizzaQuantity, pizzaSize, pizzaToppings) {
+  this.pizzaName = pizzaName;
+  this.pizzaQuantity = pizzaQuantity;
+  this.pizzaSize = pizzaSize;
+  this.pizzaToppings = pizzaToppings;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,15 @@
 
+
+describe("Pizza", function() {
+  it("creates a new pizza", function() {
+    var testPizza = new Pizza("Kellen's Pizza", 2, "L", "Sausage");
+    expect(testPizza.pizzaName).to.equal("Kellen's Pizza");
+    expect(testPizza.pizzaQuantity).to.equal(2);
+    expect(testPizza.pizzaSize).to.equal("L");
+    expect(testPizza.pizzaToppings).to.equal("Sausage");
+  });
+});
+
+
+
+//calculates the cost of a pizza

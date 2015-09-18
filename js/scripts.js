c1990e68df8b338ed47d134ce7e73a0ef761a755
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-09-18 15:20:41 -0700

    Add README, change some css styling, work on trying to pass code for adding pizza

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,31 @@ Pizza.prototype.pizzaPrice = function () {
   } else if (this.pizzaSize === "x-large") {
     pizzaPrice = 20
   }
-  return pizzaPrice
+ var totalCost = pizzaPrice;
+ return totalCost
 }
 
 
 
 
 $(document).ready(function() {
+  $("form#new-pizza-form").submit(function(event) {
+    event.preventDefault();
 
+    $("#new-pizza-form").each(function() {
+      var pizzaToppingsList = [];
+      var pizzaName = $("input#pizza-name").val();
+      var pizzaQuantity = $("select#pizza-quantity").val();
+      var pizzaSize = $("select#pizza-size").val();
+      var pizzaTopping1 = $("select#pizza-topping1").val()
+      var pizzaTopping2 = $("select#pizza-topping2").val()
+      var pizzaTopping3= $("select#pizza-topping3").val();
+      var pizzaTopping4= $("select#pizza-topping4").val();
+      // var pizzaToppings= (pizzaTopping1, pizzaTopping2, pizzaTopping3, pizzaTopping4)
+      pizzaToppingsList.push(pizzaTopping1, pizzaTopping2, pizzaTopping3, pizzaTopping4);
+      console.log(pizzaToppingsList)
+      var newPizza = (pizzaName, pizzaQuantity, pizzaSize, pizzaToppingsList)
+        $("#pizza-name-cost").text(pizzaName + " " + "will cost" + newPizza.pizzaPrice());
+    });
+  });
 });

cd907dbd02292837a273dfa96ed8e331ad0c6966
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-09-18 14:33:13 -0700

    Add spec and passing code to calculate the cost of a pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,24 @@ function Pizza(pizzaName, pizzaQuantity, pizzaSize, pizzaToppings) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
 }
+
+Pizza.prototype.pizzaPrice = function () {
+  var pizzaPrice = 0;
+  if (this.pizzaSize === "small") {
+    pizzaPrice = 5;
+  } else if (this.pizzaSize === "medium") {
+      pizzaPrice = 10;
+  } else if (this.pizzaSize === "large") {
+      pizzaPrice = 15;
+  } else if (this.pizzaSize === "x-large") {
+    pizzaPrice = 20
+  }
+  return pizzaPrice
+}
+
+
+
+
+$(document).ready(function() {
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -2,14 +2,16 @@
 
 describe("Pizza", function() {
   it("creates a new pizza", function() {
-    var testPizza = new Pizza("Kellen's Pizza", 2, "L", "Sausage");
+    var testPizza = new Pizza("Kellen's Pizza", "2", "small", "sausage");
     expect(testPizza.pizzaName).to.equal("Kellen's Pizza");
-    expect(testPizza.pizzaQuantity).to.equal(2);
-    expect(testPizza.pizzaSize).to.equal("L");
-    expect(testPizza.pizzaToppings).to.equal("Sausage");
+    expect(testPizza.pizzaQuantity).to.equal("2");
+    expect(testPizza.pizzaSize).to.equal("small");
+    expect(testPizza.pizzaToppings).to.equal("sausage");
+  });
+
+  it("calculates the cost of a pizza", function() {
+    var testPizza = new Pizza("Bill and Ted's Pizza", "1", "large", "pepperoni", "mushrooms", "onions", "");
+    expect(testPizza.pizzaPrice()).to.eql(15);
+
   });
 });
-
-
-
-//calculates the cost of a pizza
